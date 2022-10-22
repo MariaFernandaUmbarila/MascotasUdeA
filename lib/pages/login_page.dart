2dcfbc8dfdf02a66ec4e4ad538efaeff9bade171
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2022-10-22 18:15:15 -0500

    Modificaciones

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: sort_child_properties_last
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,6 +11,43 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  child: const Image(image: AssetImage("assets/logo.jpg"), width: 100, height: 100),
+                  padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+                  margin: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(200),
+                      border: Border.all(color: Colors.black, width: 4)
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Usuer",
+                    border: OutlineInputBorder(),
+                    suffixIcon: Icon(Icons.person)
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.vpn_key_sharp)
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: sort_child_properties_last
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -8,8 +10,33 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState(){
+    passPage();
+    super.initState();
+  }
+
+  Future<void> passPage() async{
+    Future.delayed(const Duration(seconds: 3), () async{
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Image(image: AssetImage("assets/logo.jpg"), width: 200, height: 200),
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+          margin: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(200),
+            border: Border.all(color: Colors.black, width: 4)
+          ),
+        ),
+      ),
+    );
   }
 }
